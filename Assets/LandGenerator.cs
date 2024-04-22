a948084f5920c1b346b13fac3d43b1a28f0f5881
Ania Borkowska <amborkowska21@gmail.com>
2024-04-22 13:58:38 +0200

    different smooting

--- Assets/LandGenerator.cs
+++ Assets/LandGenerator.cs
@@ -4,122 +4,122 @@ using Random = System.Random;
 
 public class LandGenerator : MonoBehaviour
 {
-   public int width;
-   public int height;
+    public int width;
+    public int height;
 
-   public string seed;
-   public bool useRandomSeed;
-   
-   //Determines the % of the map that will be water
-   [Range(0, 100)]
-   public int waterPercent;
-   
-   private int[,] map;
-   
-   private void Start()
-   {
-      GenerateMap();
-   }
+    public string seed;
+    public bool useRandomSeed;
 
-   void GenerateMap()
-   {
-      map = new int [width, height];
-      RandomFillMap();
+    //Determines the % of the map that will be water
+    [Range(0, 100)] public int waterPercent;
 
-      for (int i = 0; i < 5; i++)
-      {
-         SmoothMap();
-      }
-   }
+    private int[,] map;
 
-   private void RandomFillMap()
-   {
-      if (useRandomSeed)
-      {
-         seed = Time.time.ToString();
-      }
+    private void Start()
+    {
+        GenerateMap();
+    }
 
-      var pseudoRandom = new Random(seed.GetHashCode());
+    void GenerateMap()
+    {
+        map = new int [width, height];
+        RandomFillMap();
 
-      for (int x = 0; x < width; x++)
-      {
-         for (int y = 0; y < height; y++)
-         {
-            if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+        for (int i = 0; i < 5; i++)
+        {
+            SmoothMap();
+        }
+    }
+
+    private void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        var pseudoRandom = new Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
             {
-               map[x, y] = 1;
-            }
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
 
-            else
+                else
+                {
+                    map[x, y] = (pseudoRandom.Next(0, 100) < waterPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        GenerateMap();
+    }
+
+    void SmoothMap()
+    {
+        int[,] old = (int[,])map.Clone();
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
             {
-               map[x, y] = (pseudoRandom.Next(0, 100) < waterPercent) ? 1 : 0;
+                int neighborWaterTiles = GetSurroundingWaterCount(old, x, y, width, height);
+
+                if (neighborWaterTiles > 4)
+                    old[x, y] = 1;
+                else if (neighborWaterTiles < 4)
+                    old[x, y] = 0;
             }
-         }
-      }
-   }
+        }
 
-   private void OnValidate()
-   {
-      GenerateMap();
-   }
+        map = old;
+    }
 
-   void SmoothMap()
-   {
-      for (int x = 0; x < width; x++)
-      {
-         for (int y = 0; y < height; y++)
-         {
-            int neighborWaterTiles = GetSurroundingWaterCount(x, y);
+    private static int GetSurroundingWaterCount(int[,] map, int gridX, int gridY, int width, int height)
+    {
+        int waterCount = 0;
 
-            if (neighborWaterTiles > 4)
-               map[x, y] = 1;
-            else if (neighborWaterTiles < 4)
-               map[x, y] = 0;
-         }
-      }
-   }
-
-   int GetSurroundingWaterCount(int gridX, int gridY)
-   {
-      int waterCount = 0;
-
-      for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++)
-      {
-         for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++)
-         {
-            if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+        for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++)
+        {
+            for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++)
             {
-               if (neighborX != gridX || neighborY != gridY)
-               {
-                  waterCount += map[neighborX, neighborY];
-               }
-            }
-            
-            else
-            {
-               waterCount++;
-            }
-         }
-      }
+                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
+                {
+                    if (neighborX != gridX || neighborY != gridY)
+                    {
+                        waterCount += map[neighborX, neighborY];
+                    }
+                }
 
-      return waterCount;
-   }
-   
-   private void OnDrawGizmos()
-   {
-      if (map == null)
-         return;
-      
-      for (int x = 0; x < width; x++)
-      {
-         for (int y = 0; y < height; y++)
-         {
-            
-            Gizmos.color = (map[x, y] == 1) ? Color.blue : Color.green;
-            Vector2 pos = new Vector2(-width / 2 + x + 0.5f, -height / 2 + y + 0.5f);
-            Gizmos.DrawCube(pos, Vector2.one);
-         }
-      }
-   }
+                else
+                {
+                    waterCount++;
+                }
+            }
+        }
+
+        return waterCount;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (map == null)
+            return;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Gizmos.color = (map[x, y] == 1) ? Color.blue : Color.green;
+                Vector2 pos = new Vector2(-width / 2 + x + 0.5f, -height / 2 + y + 0.5f);
+                Gizmos.DrawCube(pos, Vector2.one);
+            }
+        }
+    }
 }
-
